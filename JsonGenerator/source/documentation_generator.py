d8959a880d41e7b4e6f1265c4379493e496afb2e
sebaszm <s.szczepaniak@metrological.com>
2024-07-04 10:16:24 +0200

    [JsonGen] Fix text tag in docs

--- JsonGenerator/source/documentation_generator.py
+++ JsonGenerator/source/documentation_generator.py
@@ -96,8 +96,6 @@ def Create(log, schema, path, indent_size = 4):
                             "required" in parent
                             and name not in parent["required"]) or ("required" in parent and len(parent["required"]) == 0)
 
-                name = (name if not "@originalname" in obj else obj["@originalname"].lower())
-
                 # include information about enum values in description
                 enum = ""
                 if "enum" in obj and "ids" in obj:
@@ -180,7 +178,7 @@ def Create(log, schema, path, indent_size = 4):
                         log.Warn("'%s': no 'required' field present (assuming all members optional)" % name)
 
                     for pname, props in obj["properties"].items():
-                        _TableObj(pname, props, parentName + "/" + (name if not "@originalname" in props else props["@originalname"].lower()), obj, prefix, False)
+                        _TableObj(pname, props, parentName + "/" + name, obj, prefix, False)
                 elif obj["type"] == "array":
                     _TableObj("", obj["items"], parentName + "/" + name, obj, (prefix + "[#]") if name else "", optional)
 
