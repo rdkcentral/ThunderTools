16579f4ec2356df31ab3c1caac8c42cc0f6c5b55
HaseenaSainul <h.sainul@metrological.com>
2024-09-05 05:36:21 -0400

    ThreadId namespace change

--- JsonGenerator/source/json_loader.py
+++ JsonGenerator/source/json_loader.py
@@ -385,7 +385,7 @@ class JsonThreadId(JsonNative, JsonType):
 
     @property
     def cpp_native_type(self):
-        return "Core::thread_id"
+        return "::thread_id"
 
 
 class JsonRefCounted():
