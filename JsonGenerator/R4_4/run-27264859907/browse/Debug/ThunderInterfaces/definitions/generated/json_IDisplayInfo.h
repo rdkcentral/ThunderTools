725088787ee19d5420d7cfcc6c63007f90335233
sebaszm <sebaszm@users.noreply.github.com>
2026-06-10 09:01:32 +0000

    deploy: 4888660b49e824a6ca43e2e712bac45536e1efbf

---
+++ JsonGenerator/R4_4/run-27264859907/browse/Debug/ThunderInterfaces/definitions/generated/json_IDisplayInfo.h
@@ -0,0 +1,12 @@
+// IDisplayInfo API
+// Generated automatically. DO NOT EDIT.
+
+#pragma once
+
+#include "JsonData_ConnectionProperties.h"
+#include "JsonData_HDRProperties.h"
+#include "JsonData_DisplayProperties.h"
+#include "JGraphicsProperties.h"
+#include "JConnectionProperties.h"
+#include "JHDRProperties.h"
+#include "JDisplayProperties.h"
