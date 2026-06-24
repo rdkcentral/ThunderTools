9bdc27dbe79cbb2e5dff3d73656dcdd7f9c5a7ff
sebaszm <45654185+sebaszm@users.noreply.github.com>
2026-06-24 10:35:47 +0200

    Fix text for some enums with digits (#290) (#291)

--- ProxyStubGenerator/CppParser.py
+++ ProxyStubGenerator/CppParser.py
@@ -2621,6 +2621,8 @@ def Parse(contents,log = None):
                 new_enum.meta.decorators.append("encode:text")
                 encode_enum_next = False
 
+            text_next = None
+
         # Parse class access specifier...
         elif isinstance(current_block[-1], Class) and tokens[i] == ':':
             current_block[-1]._current_access = tokens[i - 1]
@@ -2849,6 +2851,11 @@ def Parse(contents,log = None):
                         value = entry[where + 1:]
                         del entry[where:]
 
+                    if text_next:
+                        # handle case when @text precedes the first enumerator
+                        entry = entry + ["@TEXT", [text_next]]
+                        text_next = None
+
                     Enumerator(enum, entry, value, enum.type)
                     if tokens[i + 1] == '}':
                         i += 1 # handle ,} situation
@@ -2857,6 +2864,7 @@ def Parse(contents,log = None):
                         break
                     else:
                         j = i + 1
+
                 i += 1
 
             if in_typedef:
