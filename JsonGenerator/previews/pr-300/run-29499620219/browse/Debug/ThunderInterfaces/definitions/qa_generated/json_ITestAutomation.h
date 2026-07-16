440e3351b1650afa7ba56f38cbff3f3259d38a51
sebaszm <sebaszm@users.noreply.github.com>
2026-07-16 13:11:13 +0000

    deploy: 3ddc90d88902acc2bd021594f2a6e1f68dca5485

---
+++ JsonGenerator/previews/pr-300/run-29499620219/browse/Debug/ThunderInterfaces/definitions/qa_generated/json_ITestAutomation.h
@@ -0,0 +1,19 @@
+// ITestAutomation API
+// Generated automatically. DO NOT EDIT.
+
+#pragma once
+
+#include "JsonData_Memory.h"
+#include "JsonData_ComRpc.h"
+#include "JsonData_TestTextOptions.h"
+#include "JsonData_TestTextOptionsTestLegacy.h"
+#include "JsonData_TestTextOptionsTestKeep.h"
+#include "JsonData_TestTextOptionsTestCustom.h"
+#include "JsonData_TestUtils.h"
+#include "JMemory.h"
+#include "JComRpc.h"
+#include "JTestTextOptions.h"
+#include "JTestTextOptionsTestLegacy.h"
+#include "JTestTextOptionsTestKeep.h"
+#include "JTestTextOptionsTestCustom.h"
+#include "JTestUtils.h"
