8b586c439d4c0f0b29373c7c2281e4413b93d2a4
sebaszm <sebaszm@users.noreply.github.com>
2026-07-16 12:42:02 +0000

    deploy: e97f0cfe4f37dc3416b46d2aad0527119daefc34

---
+++ JsonGenerator/previews/pr-300/run-29498120304/browse/Debug/ThunderInterfaces/definitions/generated/json_IContentProtection.h
@@ -0,0 +1,9 @@
+// IContentProtection API
+// Generated automatically. DO NOT EDIT.
+
+#pragma once
+
+#include "JsonData_ContentProtection.h"
+#include "JsonData_ErrorToString.h"
+#include "JContentProtection.h"
+#include "JErrorToString.h"
