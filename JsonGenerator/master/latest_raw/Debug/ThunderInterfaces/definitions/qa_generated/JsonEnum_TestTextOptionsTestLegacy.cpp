57d45ebb68fb9f23cf7aa0a87362ecc38ed7fe1c
sebaszm <sebaszm@users.noreply.github.com>
2026-07-20 12:20:19 +0000

    deploy: 4a014f8c18dd9db8b17484360e2ddf87d02f65da

---
+++ JsonGenerator/master/latest_raw/Debug/ThunderInterfaces/definitions/qa_generated/JsonEnum_TestTextOptionsTestLegacy.cpp
@@ -0,0 +1,18 @@
+// Enumeration code for TestLegacy API.
+// Generated automatically from 'ITestAutomation.h'.
+
+#include <core/Enumerate.h>
+
+#include "definitions.h"
+#include <qa_interfaces/ITestAutomation.h>
+#include "JsonData_TestTextOptionsTestLegacy.h"
+
+namespace Thunder {
+
+ENUM_CONVERSION_BEGIN(QualityAssurance::ITestTextOptions::ITestLegacy::EnumTextOptions)
+    { QualityAssurance::ITestTextOptions::ITestLegacy::EnumTextOptions::FIRST_OPTION, _TXT("FirstOption") },
+    { QualityAssurance::ITestTextOptions::ITestLegacy::EnumTextOptions::SECOND_OPTION, _TXT("SecondOption") },
+    { QualityAssurance::ITestTextOptions::ITestLegacy::EnumTextOptions::ThirdOption, _TXT("ThirdOption") },
+ENUM_CONVERSION_END(QualityAssurance::ITestTextOptions::ITestLegacy::EnumTextOptions)
+
+}
