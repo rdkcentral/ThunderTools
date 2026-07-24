154ebd24214064a05b03536a72f4b5408908eb7a
sebaszm <sebaszm@users.noreply.github.com>
2026-07-24 08:50:25 +0000

    deploy: 5dca21fdb0b87edf4bd5f8158d26f8a8b324155b

---
+++ JsonGenerator/master/latest_raw/Debug/ThunderInterfaces/definitions/generated/JsonData_BrowserResources.h
@@ -0,0 +1,33 @@
+// C++ types for BrowserResources API.
+// Generated automatically from 'IBrowser.h'. DO NOT EDIT.
+
+// Note: This code is inherently not thread safe. If required, proper synchronisation must be added.
+
+#pragma once
+
+#include <core/JSON.h>
+#include <interfaces/IBrowser.h>
+
+namespace Thunder {
+
+namespace JsonData {
+
+    PUSH_WARNING(DISABLE_WARNING_TYPE_LIMITS)
+
+    namespace BrowserResources {
+
+        // Method params/result classes
+        //
+
+    } // namespace BrowserResources
+
+    POP_WARNING()
+
+} // namespace JsonData
+
+// Enum conversion handlers
+ENUM_CONVERSION_HANDLER(Exchange::IWebBrowser::VisibilityType)
+ENUM_CONVERSION_HANDLER(Exchange::IWebBrowser::HTTPCookieAcceptPolicyType)
+
+}
+
