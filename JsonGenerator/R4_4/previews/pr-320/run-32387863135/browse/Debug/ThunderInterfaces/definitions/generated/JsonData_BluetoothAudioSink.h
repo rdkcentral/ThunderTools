f7bfec8d21ac3b6131605e6c43a9a1caed7240fa
msmith113 <msmith113@users.noreply.github.com>
2026-08-20 15:55:01 +0000

    deploy: 75b07b035de56b15f5ca453979126f91ef7b0c89

---
+++ JsonGenerator/R4_4/previews/pr-320/run-32387863135/browse/Debug/ThunderInterfaces/definitions/generated/JsonData_BluetoothAudioSink.h
@@ -0,0 +1,357 @@
+// C++ types for BluetoothAudioSink API.
+// Generated automatically from 'IBluetoothAudio.h'. DO NOT EDIT.
+
+// Note: This code is inherently not thread safe. If required, proper synchronisation must be added.
+
+#pragma once
+
+#include <core/JSON.h>
+#include <interfaces/IBluetoothAudio.h>
+
+namespace WPEFramework {
+
+namespace JsonData {
+
+    PUSH_WARNING(DISABLE_WARNING_TYPE_LIMITS)
+
+    namespace BluetoothAudioSink {
+
+        // Method params/result classes
+        //
+
+        class AssignParamsData : public Core::JSON::Container {
+        public:
+            AssignParamsData()
+                : Core::JSON::Container()
+            {
+                Add(_T("address"), &Address);
+            }
+
+            AssignParamsData(const AssignParamsData&) = delete;
+            AssignParamsData(AssignParamsData&&) noexcept  = delete;
+
+            AssignParamsData& operator=(const AssignParamsData&) = delete;
+            AssignParamsData& operator=(AssignParamsData&&) noexcept  = delete;
+
+            ~AssignParamsData() = default;
+
+        public:
+            bool IsDataValid() const
+            {
+                return (Address.IsSet() == true);
+            }
+
+        public:
+            Core::JSON::String Address; // Address of the bluetooth device to assign
+        }; // class AssignParamsData
+
+        class CodecPropertiesData : public Core::JSON::Container {
+        public:
+            CodecPropertiesData()
+                : Core::JSON::Container()
+            {
+                _Init();
+            }
+
+            CodecPropertiesData(const CodecPropertiesData& _other)
+                : Core::JSON::Container()
+                , Codec(_other.Codec)
+                , Settings(_other.Settings)
+            {
+                _Init();
+            }
+
+            CodecPropertiesData(CodecPropertiesData&& _other) noexcept
+                : Core::JSON::Container()
+                , Codec(std::move(_other.Codec))
+                , Settings(std::move(_other.Settings))
+            {
+                _Init();
+            }
+
+            CodecPropertiesData(const Exchange::IBluetoothAudioSink::CodecProperties& _other)
+                : Core::JSON::Container()
+            {
+                Codec = _other.Codec;
+                Settings = _other.Settings;
+                _Init();
+            }
+
+            CodecPropertiesData& operator=(const CodecPropertiesData& _rhs)
+            {
+                Codec = _rhs.Codec;
+                Settings = _rhs.Settings;
+                return (*this);
+            }
+
+            CodecPropertiesData& operator=(CodecPropertiesData&& _rhs) noexcept
+            {
+                Codec = std::move(_rhs.Codec);
+                Settings = std::move(_rhs.Settings);
+                return (*this);
+            }
+
+            CodecPropertiesData& operator=(const Exchange::IBluetoothAudioSink::CodecProperties& _rhs)
+            {
+                Codec = _rhs.Codec;
+                Settings = _rhs.Settings;
+                return (*this);
+            }
+
+            operator Exchange::IBluetoothAudioSink::CodecProperties() const
+            {
+                Exchange::IBluetoothAudioSink::CodecProperties _value{};
+                _value.Codec = Codec;
+                _value.Settings = Settings;
+                return (_value);
+            }
+
+            ~CodecPropertiesData() = default;
+
+        public:
+            bool IsDataValid() const
+            {
+                return ((Codec.IsSet() == true) && (Settings.IsSet() == true));
+            }
+
+        private:
+            void _Init()
+            {
+                Add(_T("codec"), &Codec);
+                Add(_T("settings"), &Settings);
+            }
+
+        public:
+            Core::JSON::EnumType<Exchange::IBluetoothAudioSink::audiocodec> Codec; // Audio codec used
+            Core::JSON::String Settings; // Codec-specific audio quality preset, compression profile, etc
+        }; // class CodecPropertiesData
+
+        class DRMPropertiesData : public Core::JSON::Container {
+        public:
+            DRMPropertiesData()
+                : Core::JSON::Container()
+            {
+                _Init();
+            }
+
+            DRMPropertiesData(const DRMPropertiesData& _other)
+                : Core::JSON::Container()
+                , DRM(_other.DRM)
+                , Settings(_other.Settings)
+            {
+                _Init();
+            }
+
+            DRMPropertiesData(DRMPropertiesData&& _other) noexcept
+                : Core::JSON::Container()
+                , DRM(std::move(_other.DRM))
+                , Settings(std::move(_other.Settings))
+            {
+                _Init();
+            }
+
+            DRMPropertiesData(const Exchange::IBluetoothAudioSink::DRMProperties& _other)
+                : Core::JSON::Container()
+            {
+                DRM = _other.DRM;
+                Settings = _other.Settings;
+                _Init();
+            }
+
+            DRMPropertiesData& operator=(const DRMPropertiesData& _rhs)
+            {
+                DRM = _rhs.DRM;
+                Settings = _rhs.Settings;
+                return (*this);
+            }
+
+            DRMPropertiesData& operator=(DRMPropertiesData&& _rhs) noexcept
+            {
+                DRM = std::move(_rhs.DRM);
+                Settings = std::move(_rhs.Settings);
+                return (*this);
+            }
+
+            DRMPropertiesData& operator=(const Exchange::IBluetoothAudioSink::DRMProperties& _rhs)
+            {
+                DRM = _rhs.DRM;
+                Settings = _rhs.Settings;
+                return (*this);
+            }
+
+            operator Exchange::IBluetoothAudioSink::DRMProperties() const
+            {
+                Exchange::IBluetoothAudioSink::DRMProperties _value{};
+                _value.DRM = DRM;
+                _value.Settings = Settings;
+                return (_value);
+            }
+
+            ~DRMPropertiesData() = default;
+
+        public:
+            bool IsDataValid() const
+            {
+                return ((DRM.IsSet() == true) && (Settings.IsSet() == true));
+            }
+
+        private:
+            void _Init()
+            {
+                Add(_T("drm"), &DRM);
+                Add(_T("settings"), &Settings);
+            }
+
+        public:
+            Core::JSON::EnumType<Exchange::IBluetoothAudioSink::drmscheme> DRM; // Content protection scheme used
+            Core::JSON::String Settings; // DRM-specific content protection level, encoding rules, etc
+        }; // class DRMPropertiesData
+
+        class LatencyData : public Core::JSON::Container {
+        public:
+            LatencyData()
+                : Core::JSON::Container()
+            {
+                Add(_T("value"), &Value);
+            }
+
+            LatencyData(const LatencyData&) = delete;
+            LatencyData(LatencyData&&) noexcept  = delete;
+
+            LatencyData& operator=(const LatencyData&) = delete;
+            LatencyData& operator=(LatencyData&&) noexcept  = delete;
+
+            ~LatencyData() = default;
+
+        public:
+            bool IsDataValid() const
+            {
+                return (Value.IsSet() == true);
+            }
+
+        public:
+            Core::JSON::DecSInt16 Value; // Audio latency of the sink in milliseconds
+        }; // class LatencyData
+
+        class StreamPropertiesData : public Core::JSON::Container {
+        public:
+            StreamPropertiesData()
+                : Core::JSON::Container()
+            {
+                _Init();
+            }
+
+            StreamPropertiesData(const StreamPropertiesData& _other)
+                : Core::JSON::Container()
+                , SampleRate(_other.SampleRate)
+                , BitRate(_other.BitRate)
+                , Channels(_other.Channels)
+                , Resolution(_other.Resolution)
+                , IsResampling(_other.IsResampling)
+            {
+                _Init();
+            }
+
+            StreamPropertiesData(StreamPropertiesData&& _other) noexcept
+                : Core::JSON::Container()
+                , SampleRate(std::move(_other.SampleRate))
+                , BitRate(std::move(_other.BitRate))
+                , Channels(std::move(_other.Channels))
+                , Resolution(std::move(_other.Resolution))
+                , IsResampling(std::move(_other.IsResampling))
+            {
+                _Init();
+            }
+
+            StreamPropertiesData(const Exchange::IBluetoothAudioSink::StreamProperties& _other)
+                : Core::JSON::Container()
+            {
+                SampleRate = _other.SampleRate;
+                BitRate = _other.BitRate;
+                Channels = _other.Channels;
+                Resolution = _other.Resolution;
+                IsResampling = _other.IsResampling;
+                _Init();
+            }
+
+            StreamPropertiesData& operator=(const StreamPropertiesData& _rhs)
+            {
+                SampleRate = _rhs.SampleRate;
+                BitRate = _rhs.BitRate;
+                Channels = _rhs.Channels;
+                Resolution = _rhs.Resolution;
+                IsResampling = _rhs.IsResampling;
+                return (*this);
+            }
+
+            StreamPropertiesData& operator=(StreamPropertiesData&& _rhs) noexcept
+            {
+                SampleRate = std::move(_rhs.SampleRate);
+                BitRate = std::move(_rhs.BitRate);
+                Channels = std::move(_rhs.Channels);
+                Resolution = std::move(_rhs.Resolution);
+                IsResampling = std::move(_rhs.IsResampling);
+                return (*this);
+            }
+
+            StreamPropertiesData& operator=(const Exchange::IBluetoothAudioSink::StreamProperties& _rhs)
+            {
+                SampleRate = _rhs.SampleRate;
+                BitRate = _rhs.BitRate;
+                Channels = _rhs.Channels;
+                Resolution = _rhs.Resolution;
+                IsResampling = _rhs.IsResampling;
+                return (*this);
+            }
+
+            operator Exchange::IBluetoothAudioSink::StreamProperties() const
+            {
+                Exchange::IBluetoothAudioSink::StreamProperties _value{};
+                _value.SampleRate = SampleRate;
+                _value.BitRate = BitRate;
+                _value.Channels = Channels;
+                _value.Resolution = Resolution;
+                _value.IsResampling = IsResampling;
+                return (_value);
+            }
+
+            ~StreamPropertiesData() = default;
+
+        public:
+            bool IsDataValid() const
+            {
+                return ((SampleRate.IsSet() == true) && (BitRate.IsSet() == true) && (Channels.IsSet() == true) && (Resolution.IsSet() == true) && (IsResampling.IsSet() == true));
+            }
+
+        private:
+            void _Init()
+            {
+                Add(_T("samplerate"), &SampleRate);
+                Add(_T("bitrate"), &BitRate);
+                Add(_T("channels"), &Channels);
+                Add(_T("resolution"), &Resolution);
+                Add(_T("isresampling"), &IsResampling);
+            }
+
+        public:
+            Core::JSON::DecUInt32 SampleRate; // Sample rate in Hz
+            Core::JSON::DecUInt32 BitRate; // Target bitrate in bits per second (eg. 320000)
+            Core::JSON::DecUInt8 Channels; // Number of audio channels
+            Core::JSON::DecUInt8 Resolution; // Sampling resolution in bits per sample
+            Core::JSON::Boolean IsResampling; // Indicates if the sink is resampling the input stream
+        }; // class StreamPropertiesData
+
+    } // namespace BluetoothAudioSink
+
+    POP_WARNING()
+
+} // namespace JsonData
+
+// Enum conversion handlers
+ENUM_CONVERSION_HANDLER(Exchange::IBluetoothAudioSink::state)
+ENUM_CONVERSION_HANDLER(Exchange::IBluetoothAudioSink::devicetype)
+ENUM_CONVERSION_HANDLER(Exchange::IBluetoothAudioSink::audiocodec)
+ENUM_CONVERSION_HANDLER(Exchange::IBluetoothAudioSink::drmscheme)
+
+}
+
---
+++ JsonGenerator/R4_4/previews/pr-320/run-32387863135/browse/Debug/ThunderInterfaces/definitions/generated/json_IOConnector.h
@@ -0,0 +1,7 @@
+// IOConnector API
+// Generated automatically. DO NOT EDIT.
+
+#pragma once
+
+#include "JsonData_IOConnector.h"
+#include "JIOConnector.h"
